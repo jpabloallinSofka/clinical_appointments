97e206843e832f9b993f5c116d632491ac3da90c
Juan Pablo Allin Cañas <juan.allin@sofka.com.co>
2024-03-04 13:13:33 -0500

    initial commit

---
+++ src/main/java/com/medical/appointments/MedicalAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.medical.appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentsApplication.class, args);
+	}
+
+}
